7238497b2faa2e0138ac0eef5065e40189e6714e
FuJiT8T <fujitoit@gmail.com>
2025-07-26 17:50:35 +0300

    Rewritten TODO

--- Main.dart
+++ Main.dart
@@ -3,7 +3,11 @@ import 'TODO/Halo.dart';
 import 'TODO/options.dart';
 
 void main() {
-  stdout.write('\x1B[8;${64};${30}t');
+  void setConsoleSize(int height, int width) {
+    // ANSI escape code to set terminal size (works in most Unix-like terminals)
+    stdout.write('\x1B[8;1000;100t');
+  }
   Halo.Halo1();
+  Halo.Clear();
   Options.options1();
 }
--- TODO/Options.dart
+++ TODO/Options.dart
@@ -1,35 +1,74 @@
 import 'dart:io';
+import 'ListString.dart';
+import 'Options.dart';
 
 class Options{
-    static List<String> tasks = [];
-    static void options1(){
+    static void options1() async{
         print('1.see all the tasks');
         print('2.add a task');
-        print('3.remove a task');
-        print('4.sort the tasks from a-z');
+        print('3.rewrite the tasks');
+        print('4.remove a task');
+        print('5.sort the tasks from a-z');
         var input = stdin.readLineSync()!;
         int input1 = int.tryParse(input)!;
         if(input1 == 1)
         {
-            print(tasks);
+            final file = File('TODO.txt');
+            final content = file.readAsStringSync();
+            print (content);
         } 
         else if(input1 == 2)
         {
             print('What task would you like to add in here');
-            String? b = stdin.readLineSync();
-            tasks.add(b!);      
+            final file = File('TODO.txt');
+            String? a = stdin.readLineSync();
+            file.writeAsStringSync('\n'+ a!, mode:FileMode.append);
+            print('You have succefully added new task');
         }
         else if (input1 == 3)
         {
-            print('What would like to delete?');
-            var rem = stdin.readLineSync()!;
-            int rem1 = int.tryParse(rem)!;
-            int rem2 = rem1 - 1;
-            tasks.removeAt(rem2);
+            print('Rewrite something already written');
+            final file = File('TODO.txt');
+            String? a = stdin.readLineSync();
+            file.writeAsStringSync(a!);
+            print('You have succefully rewrite');
         }
-        else
+            else if(input1 == 4){
+            final file = File('TODO.txt');
+            List<String> lines = await file.readAsLines();
+            print('What number of the line you want to remove?');
+            int? a = int.tryParse(stdin.readLineSync()!);
+            lines.removeAt(a! - 1);
+            await file.writeAsString(''.trim());
+            for(int i=0; i < lines.length; i++){
+            if (lines[i] == null || lines[i] == ''){
+                lines.removeAt(i);
+            }
+            file.writeAsStringSync(lines[i], mode:FileMode.append);
+            if(lines.length - 1 > i){
+                file.writeAsStringSync('\n', mode:FileMode.append);
+            }
+            }
+        }
+        else if (input1 == 5)
         {
-            tasks.sort();
+            final file = File('TODO.txt');
+            List<String> lines = await file.readAsLines();
+            lines.sort();
+            await file.writeAsString(''.trim());
+            for(int i=0; i < lines.length; i++){
+            if (lines[i] == null || lines[i] == ''){
+                lines.removeAt(i);
+            }
+            file.writeAsStringSync(lines[i], mode:FileMode.append);
+            if(lines.length - 1 > i){
+                file.writeAsStringSync('\n', mode:FileMode.append);
+            }
+            }
+        }
+        else{
+            print('Introduce the correct answer');
+            options1();
         }
         options1();
     }
