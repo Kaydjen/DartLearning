7130135dce43803bdfd95980f7308051ec0f89db
FuJiT8T <fujitoit@gmail.com>
2025-08-01 22:06:29 +0300

    Map_2

--- Word_counter.dart
+++ Word_counter.dart
@@ -1,6 +1,13 @@
 class Word_counter{
     static void Words(){
-        String? ghg = "Fuad loves cookies";
-        print (ghg.split(" "));
+        String? abc = "A dog with a Cat";
+        abc = abc.toLowerCase();
+        List<String> abcList = abc.split(" ");
+        print(abcList);
+        Map<String, int> WordNumber = Map();
+        for(var dog in abcList){
+            if(abcList.isEmpty) continue;
+            abcList[dog] = (abcList[dog] ?? 0) + 1;
+        }
     }
 }
