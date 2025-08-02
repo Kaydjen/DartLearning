846521f590f4dc455fc7450f77f97e8067f56496
FuJiT8T <fujitoit@gmail.com>
2025-08-02 13:12:46 +0300

    Map 2.1(the word counter is done)

--- Main.dart
+++ Main.dart
@@ -3,12 +3,12 @@ import 'TODO/Halo.dart';
 import 'TODO/options.dart';
 import 'Maps.dart';
 import 'dart:math';
-import 'Word_counter.dart';
+import 'WordCounter.dart';
 
 void main() {
   void setConsoleSize(int height, int width) {
     // ANSI escape code to set terminal size (works in most Unix-like terminals)
     stdout.write('\x1B[8;1000;100t');
   }
-    Word_counter.Words();
+    WordCounter.words();
 }
