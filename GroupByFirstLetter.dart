26d46b98c75096e5796328d1b11945dde98cc9c7
FuJiT8T <fujitoit@gmail.com>
2025-08-03 11:51:29 +0300

    Map 2.2(Sorting by first letter(Must add an ?? 0 verifier))

--- GroupByFirstLetter.dart
+++ GroupByFirstLetter.dart
@@ -1,6 +1,6 @@
 class GroupByFirstLetter {
     static void Letter(){
-        String a = 'Fuad Kirill Alla Alina Vanya Losha Hyu';
+        String a = 'Fuad Kirill Alla Alina Vanya Losha Hyu "suka pizda" naxyu hyu skvanshi tipa you';
         List<String> b = a.toLowerCase().replaceAll(RegExp(r'[^\w\s]'), ' ').split(' ');
         Map<String, List<String>> c = Map();
         for(String name in b)
@@ -8,7 +8,11 @@ class GroupByFirstLetter {
             if(name.isEmpty) continue;
             String firstLetter = name[0];
 
-            if(c.containsKey(firstLetter)) c[firstLetter] = c.putIfAbsent(firstLetter, () => name);
+            c.putIfAbsent(firstLetter, () => []);
+            c[firstLetter]!.add(name);
         }
+        print(c);
     }
 }
+
+//if(c.containsKey(firstLetter)) c[firstLetter] = c.putIfAbsent(firstLetter, () => name);
